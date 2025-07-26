89349bbbd417288961fdd2cead6a25cdb0396f5a
keronshb <keronshb@live.com>
2025-07-26 18:25:06 -0400

    Adds ToggleableArmorSpacingProtection ECS

---
+++ Content.Shared/Armor/ToggleableArmorSpacingProtectionComponent.cs
@@ -0,0 +1,12 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared.Armor;
+
+[RegisterComponent, NetworkedComponent, Access(typeof(ToggleableArmorSpacingProtectionSystem))]
+public sealed partial class ToggleableArmorSpacingProtectionComponent : Component
+{
+    // TODO: Armor Components to toggle
+    // TODO: Space Protection Components to toggle
+    // TODO: Movement Modifier components to toggle
+    // TODO: Action to grant
+}
---
+++ Content.Shared/Armor/ToggleableArmorSpacingProtectionSystem.cs
@@ -0,0 +1,16 @@
+﻿namespace Content.Shared.Armor;
+
+public sealed class ToggleableArmorSpacingProtectionSystem : EntitySystem
+{
+    // TODO: Depdendencies
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        // TODO: Subscribes
+    }
+
+    // TODO: Grant toggle action
+    // TODO: Have toggle action effect attached clothing piece
+}
