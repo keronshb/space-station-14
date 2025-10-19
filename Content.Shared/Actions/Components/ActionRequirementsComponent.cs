35f8fd633dd473933876f157c283bd4fe4bd48fd
keronshb <keronshb@live.com>
2025-10-19 19:07:24 -0400

    Updated comments

--- Content.Shared/Actions/Components/ActionRequirementsComponent.cs
+++ Content.Shared/Actions/Components/ActionRequirementsComponent.cs
@@ -16,6 +16,11 @@ public sealed partial class ActionRequirementsComponent :  Component
     //      Like how Charges/Cooldown shows, this can update the UI info to add a line that says something like
     //      "Requires Wizard Clothes in Head and Suit slots" or something
 
+    // TODO: Maybe some sort of red X if it can't be used?
+
+    // TODO: Should this just see if it passes requirements or do a behavior?
+    // Like if it doesn't pass the requirements, do we just cancel OR have it fire off some other thing that would modify cooldowns, etc?
+
     /// <summary>
     /// Which inventory slots do the <see cref="Requirements"/> apply to?
     /// Only really applicable if the component/tags listed in <see cref="Requirements"/> is an item
@@ -34,4 +39,10 @@ public sealed partial class ActionRequirementsComponent :  Component
     // TODO: Why not add some sort of event handler like the magic/action system?
     // Generic check CompEvent, where it checks to see if the action or the performer has the comp & specified value
     //  before casting?
+
+    // TODO: Maybe add a simple event check?
+    //  Event would be for custom logic
+    //  So something like CheckCustomActionRequirements? It would supercede the other checks.
+    //  Would run this and the others off of the ActionAttemptEvent?
+    //  ECS => ActionAttemptEvent Subscribe => Run Checks (custom and non custom?) if either are cancelled, then cancel action attempt.
 }
